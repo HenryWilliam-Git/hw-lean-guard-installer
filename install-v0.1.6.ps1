14a553a0562aeb1cf2c56d3b23c5294087071eb1
Brad Agent <brad.agent@hwdev.com.au>
2026-09-02 14:31:48 +1000

    fix(release): bind v0.1.6 secure bundle

--- install-v0.1.6.ps1
+++ install-v0.1.6.ps1
@@ -13,7 +13,7 @@ $packageVersion = '0.1.6'
 $packageName = 'HW-Lean-Guard-v0.1.6-Innessco.zip'
 $packageRootName = 'HW-Lean-Guard-v0.1.6-Innessco'
 $packageUri = 'https://github.com/HenryWilliam-Git/hw-lean-guard-installer/releases/download/v0.1.6/HW-Lean-Guard-v0.1.6-Innessco.zip'
-$expectedPackageSha256 = 'adc4ea283a30d53e7b15aceb15339311ff71d74e4791aedca5f6aa2d100ebe68'
+$expectedPackageSha256 = 'bb954e7f02da4b9892d8710c9cb8faa59b2ce7f6ffbe48477036e96ddb1efc6e'
 
 function Assert-SafeLeanGuardArchive {
   param(
